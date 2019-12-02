a83bd52dd69f5b96042eaefd050784eb85619e2b
nearprosmith <is@age.co.jp>
2019-12-02 16:52:34 +0900

    Remove unnecessary example file

--- example/login-twitter2.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-require __DIR__ . '/../vendor/autoload.php';
-
-$chrome = new \HeadlessChrome\Chrome(null, 9222);
-$page = $chrome->getPage(0);
-$page->moveTo('https://twitter.com/login');
